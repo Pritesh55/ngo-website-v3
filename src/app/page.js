b126f264f0f5e710c214804bea6e267d73d9400b
Pritesh B. Shah <iampritesh13@gmail.com>
2026-06-04 11:11:19 +0530

    Create Nextjs App
    <# 01 #> npm  npx create-next-app ngo-website-v3;
    <# 02 #> cd .\ngo-website-v3\;
    <# 03 #> npm run dev;
    
    Github-desktop => Menubar => File --> New repository ::
    <# 01 #> npm install;
    <# 03 #> npm run dev;

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-6xl text-center py-48">
+        Welcome to NGO website...
+      </h1>
+
+    </>
+  );
+}
