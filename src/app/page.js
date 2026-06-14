e9724b3ee14a8c268912f3de09f7704b011370c5
Pritesh B. Shah <iampritesh13@gmail.com>
2026-06-14 17:48:21 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Our_team from "@/components/sections/Our_team";
 import CTA_Donate_or_vol from "@/components/sections/CTA_Donate_or_vol";
 import Our_Projects_Cards_Section from "@/components/sections/our_projects_cards_section";
 import Award_Cards_Section from "@/components/sections/Award_Cards_Section";
+import PartnerSection from "@/components/sections/our-partners";
 
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
       <Our_Cource_Cards_Section></Our_Cource_Cards_Section>
       <Our_team />
       <Award_Cards_Section></Award_Cards_Section>
+      <PartnerSection></PartnerSection>
       <CTA_Donate_or_vol></CTA_Donate_or_vol>
       <Contact_us_Main_Section></Contact_us_Main_Section>
 
